a517db4c9da7616d4dc485e74e27b0329b15b89f
anthonysale <anthony@jumblehome.co.uk>
2026-05-08 09:42:24 +0100

    Initial development

---
+++ iOS/ChessAccesibilityClock/ChessAccesibilityClock/ChessAccesibilityClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAccesibilityClockApp.swift
+//  ChessAccesibilityClock
+//
+//  Created by Anthony Sale on 07/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChessAccesibilityClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
